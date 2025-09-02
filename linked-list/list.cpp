372696fbd30682cc7d890dd427ea23108b0000be
Jeevan Shah <jedimaster3284@gmail.com>
2025-09-02 12:20:13 -0400

    more linked list functions

--- linked-list/list.cpp
+++ linked-list/list.cpp
@@ -22,6 +22,31 @@ class LinkedList {
       head = newNode;
     }
 
+    void insertAt(int key, int data) {
+      Node* temp = head; 
+
+      if (temp != nullptr && temp->data == key) { // if the data corresponds to the head, simply call the insert function
+        insert(data);
+        return;
+      }
+
+      Node* newNode = new Node(data);
+      Node* prev = nullptr;
+
+      // otherwise, iterate through the list to find the key
+
+      while (temp != nullptr && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+      }
+
+      if (temp == nullptr) return; // if we don't find the key, don't modify the list
+
+      // insert the new data and adjust the pointers accordingly
+      newNode->next = temp;
+      prev->next = newNode;
+    }
+
     // delete a node given a key
     void deleteNode(int key) {
       Node* temp = head;
@@ -54,6 +79,21 @@ class LinkedList {
       }
       cout << endl;
     }
+
+    void reverse() {
+      Node* prev = nullptr;
+      Node* current = head;
+      Node* next = nullptr;
+
+      while (current != nullptr) { 
+        next = current->next;
+        current->next = prev;
+        prev=current;
+        current=next;
+      }
+      head=prev;
+      return;
+    }
 };
 
 int main() {
@@ -61,9 +101,16 @@ int main() {
   list.insert(1);
   list.insert(2);
   list.insert(3);
+
+  list.insertAt(3, 5);
   list.printList();
 
-  list.deleteNode(2);
+  list.insertAt(5, 0);
+
+  list.printList();
+
+  list.reverse();
+
   list.printList();
 
   return 0;
